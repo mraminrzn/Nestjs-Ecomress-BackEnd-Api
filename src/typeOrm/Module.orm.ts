a7315b59cee292d0fc3b1f40e9eeaf7e18fe9b58
unknown <mohammadrzn2005@gmail.com>
2025-08-14 07:21:34 +0330

    sync changed

--- src/typeOrm/Module.orm.ts
+++ src/typeOrm/Module.orm.ts
@@ -4,16 +4,18 @@ import { GeneralProduct } from './Schemas/product/general.product.entity';
 import { Review } from './Schemas/reviews.entity';
 import { GeneralCarts } from './Schemas/carts/general.orders.entity';
 import { GeneralOrder } from './Schemas/orders/general.order.entity';
+import { ConfigService } from '@nestjs/config';
 
-const { DB_NAME, DB_PORT, DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+const { DB_NAME, DB_PORT, DB_HOST } = process.env;
 
 export const DataBaseInit = TypeOrmModule.forRootAsync({
-  useFactory: () => ({
+  inject:[ConfigService],
+  useFactory: (ConfigService: ConfigService) => ({
     type: 'postgres',
     host: DB_HOST,
     port: DB_PORT ? +DB_PORT : 5432,
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
+    username: ConfigService.get('DB_USERNAME'),
+    password: ConfigService.get('DB_PASSWORD'),
     database: DB_NAME,
     entities: [User, ...GeneralProduct, Review, ...GeneralCarts, ...GeneralOrder],
     synchronize: true,
